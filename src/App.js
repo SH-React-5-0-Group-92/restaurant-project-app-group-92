c0cb0728709c349101696642824fef7deef7b12e
LolaDeveloper <82631715+LolaDeveloper@users.noreply.github.com>
2022-04-25 06:05:56 +0900

    Installed React router and imported into the App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Router>
+      <Route exact path="/" element={<Home />}></Route>
+      <Route exact path="/components/About" element={<About />}></Route>
+    </Router>
+  );
 }
 
 export default App;

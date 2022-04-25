e85edd91f7d4be749176cb38c170a7a50e487042
LolaDeveloper <82631715+LolaDeveloper@users.noreply.github.com>
2022-04-26 07:55:11 +0900

    App.js updated and Footer component created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
+import Navbar from "./components/Navbar";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import "./App.css";
 
 function App() {
   return (
     <Router>
-      <Route exact path="/" element={<Home />}></Route>
-      <Route exact path="/components/About" element={<About />}></Route>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Home />}></Route>
+        <Route exact path="/About" element={<About />}></Route>
+        <Route exact path="/Products" element={<Products />}></Route>
+      </Routes>
     </Router>
   );
 }

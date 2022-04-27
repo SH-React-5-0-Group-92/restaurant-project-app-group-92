d8d399d248efafff6d47e3c18e040f57a2d9b265
LolaDeveloper <82631715+LolaDeveloper@users.noreply.github.com>
2022-04-27 23:58:27 +0900

    updated Footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,23 @@
 import React from "react";
 
 function Footer() {
-  return <h1>Footer in progress</h1>;
+  return (
+    <footer className="container text-center">
+      <p>
+        {" "}
+        Coded by SH React-5.0 Group _92_
+        <a
+          href="https://github.com/SH-React-5-0-Group-92/restaurant-project-app-group-92"
+          title="view source code"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {" "}
+          Project Open-source
+        </a>
+      </p>
+    </footer>
+  );
 }
 
 export default Footer;

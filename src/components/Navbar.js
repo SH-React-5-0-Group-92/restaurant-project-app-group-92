a51873e31b2e12668007902df424a8abf9a73dd5
LolaDeveloper <82631715+LolaDeveloper@users.noreply.github.com>
2022-04-27 23:29:39 +0900

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./Navbar.css";
 
 function Navbar() {
-  return <div>
-    <li><Link to="/Product">Product</Link></li>
-  </div>;
+  return (
+    <div className="Navbar">
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/">Products</Link>
+        </li>
+        <li>
+          <Link to="/about">About Us</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact Us</Link>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 export default Navbar;

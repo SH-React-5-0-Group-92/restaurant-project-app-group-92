223bab020ac96cbeefabcc347c28ca742d807ce1
LolaDeveloper <82631715+LolaDeveloper@users.noreply.github.com>
2022-04-28 06:40:58 +0900

    Update HTOCard.js

--- src/components/productPageFiles/HTOCard.js
+++ src/components/productPageFiles/HTOCard.js
@@ -1,60 +1,65 @@
-import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { solid, regular, brands } from '@fortawesome/fontawesome-svg-core/import.macro'
+import React from "react";
 
-
-const cardInfo=[
+const cardInfo = [
   {
-    icon:"solid('user-secret')",
+    icon: "solid('user-secret')",
     step: "Sign-Up",
-    procedure: "You have to createan account first to order your meals convinently and have it delivered"
+    procedure:
+      "You have to createan account first to order your meals convinently and have it delivered",
   },
   {
-    icon:"vgh",
+    icon: "vgh",
     step: "Order",
-    procedure: "You have to createan account first to order your meals convinently and have it delivered"
+    procedure:
+      "You have to createan account first to order your meals convinently and have it delivered",
   },
   {
-    icon:"vgh",
+    icon: "vgh",
     step: "Set Location",
-    procedure: "You have to createan account first to order your meals convinently and have it delivered"
+    procedure:
+      "You have to createan account first to order your meals convinently and have it delivered",
   },
   {
-    icon:"vgh",
+    icon: "vgh",
     step: "Payment",
-    procedure: "You have to createan account first to order your meals convinently and have it delivered"
+    procedure:
+      "You have to createan account first to order your meals convinently and have it delivered",
   },
+];
 
-]
-
-const Card1 =(props)=>{
-  return(
-    <div className="col-lg-3 col-md-6 py-5 px-3 text-center mx-4" style={{ borderRadius: "10px", width: "220px", backgroundColor:"#fff"}}>
+const Card1 = (props) => {
+  return (
+    <div
+      className="col-lg-3 col-md-6 py-5 px-3 text-center mx-4"
+      style={{ borderRadius: "10px", width: "220px", backgroundColor: "#fff" }}
+    >
       <i className={props.icons}></i>
       <FontAwesomeIcon icon={props.icon} />
       <h3>{props.steps}</h3>
       <p>{props.procedure}</p>
     </div>
-  )
-}
+  );
+};
 
-function cardMaper(items){
-  return <Card1 
-  key={items.step}
-  icons={items.icon}
-  steps={items.step}
-  procedure={items.procedure}
-  />
+function cardMaper(items) {
+  return (
+    <Card1
+      key={items.step}
+      icons={items.icon}
+      steps={items.step}
+      procedure={items.procedure}
+    />
+  );
 }
 
 const HTOCard = () => {
   return (
-    <div className="container-fluild" style={{ backgroundColor:"#d6d6c2"}}>
-    <div className="row justify-content-center align-items-center py-5">
-      {cardInfo.map(cardMaper)}
+    <div className="container-fluild" style={{ backgroundColor: "#d6d6c2" }}>
+      <div className="row justify-content-center align-items-center py-5">
+        {cardInfo.map(cardMaper)}
+      </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default HTOCard
+export default HTOCard;

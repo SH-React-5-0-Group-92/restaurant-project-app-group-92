4255aa5aef84385b8732f994ba4b8de3a33a4336
LolaDeveloper <82631715+LolaDeveloper@users.noreply.github.com>
2022-04-29 06:36:47 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
+import About from "./components/About";
 import Products from "./components/Products";
 
 function App() {
